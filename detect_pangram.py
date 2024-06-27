f3d929b13b23ef664a2faf98c0bab601c81dfe51
Micha <mm.28@outlook.com>
2024-06-27 18:10:11 +0200

    initial commit

---
+++ detect_pangram.py
@@ -0,0 +1,23 @@
+pangrams = [
+    "The quick brown fox jumps over the lazy dog.",
+    "Cwm fjord bank glyphs vext quiz",
+    "Pack my box with five dozen liquor jugs.",
+    "How quickly daft jumping zebras vex.",
+    "ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ",
+]
+
+non_pangrams = [
+    "This isn't a pangram!",
+    "abcdefghijklm opqrstuvwxyz",
+    "Aacdefghijklmnopqrstuvwxyz",
+]
+
+
+def is_pangram():
+
+    for item in pangrams:
+        print(item)
+
+
+if __name__ == "__main__":
+    is_pangram()
